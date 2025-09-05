8df1a68a50cc7edfa5d6067c4feb05678024ae1a
Tamir <tamirhaldlevi@gmail.com>
2025-09-05 15:34:39 +0200

    m

--- src/DiceMan/DiceMan.java
+++ src/DiceMan/DiceMan.java
@@ -1,4 +1,32 @@
 package DiceMan;
 
-public class DiceMan {
-}
+public class DiceMan; {
+    String[] activities ("")
+        }
+
+
+
+switch (rollDice) {
+    case 1:
+        System.out.println(activites[0]);
+        break;
+    case 2:
+        System.out.println(activities[1]);
+        break;
+    case 3:
+        System.out.println(activities[2]);
+        break;
+    case 4:
+        System.out.println(activites[3]);
+        break;
+    case 5:
+    System.out.println(activities[4]);
+        break;
+    case 6:
+    System.out.println(activities[2]);
+        break;
+        }
+
+
+// Attributes: name (String), numberOfActivities (int).
+// Switch
