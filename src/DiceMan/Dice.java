ff5bc677aade181cdc4998aea44775f003651692
Tamir <tamirhaldlevi@gmail.com>
2025-09-05 15:34:39 +0200

    m

--- src/DiceMan/Dice.java
+++ src/DiceMan/Dice.java
@@ -2,3 +2,5 @@ package DiceMan;
 
 public class Dice {
 }
+
+// A method rollDice() that returns a random number between 1–6.
--- src/DiceMan/DiceManTest.java
+++ src/DiceMan/DiceManTest.java
@@ -1,8 +1,4 @@
 package DiceMan;
 
 public class DiceManTest {
-    public static void main(String[] args) {
-        Dice roll = new Dice(5);
-        dice.roll();
-    }
 }
